64a51ff67fc6aa51370c95cf4d773dcd40929438
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2025-09-02 20:58:30 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<script setup>
+  import ClockDisplay from './components/ClockDisplay.vue';
+</script>
+
+<template>
+  <header class="pt-20 pb-10">
+    <h1 class="text-center text-4xl font-capsule tracking-widest">
+      Universal Watch
+    </h1>
+  </header>
+
+  <main class="mt-10 mb-20 px-8">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-20">
+      <div class="flex flex-col items-center justify-center gap-6 col-span-full">
+        <ClockDisplay class="text-white" time-zone="PST">
+        </ClockDisplay>
+        <h2 class="text-white text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          {{ currentTimezone }} Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-red-700" time-zone="UTC">
+        </ClockDisplay>
+        <h2 class="text-red-700 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          UTC Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-yellow-500" time-zone="EST">
+        </ClockDisplay>
+        <h2 class="text-yellow-500 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          EST Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-emerald-500" time-zone="PST">
+        </ClockDisplay>
+        <h2 class="text-emerald-500 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          PST Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-sky-500" time-zone="GMT">
+        </ClockDisplay>
+        <h2 class="text-sky-500 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          GMT Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-purple-500" time-zone="CET">
+        </ClockDisplay>
+        <h2 class="text-purple-500 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          CET Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-rose-500" time-zone="Asia/Dubai">
+        </ClockDisplay>
+        <h2 class="text-rose-500 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          GST Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-orange-700" time-zone="IST">
+        </ClockDisplay>
+        <h2 class="text-orange-700 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          IST Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-lime-400" time-zone="CST">
+        </ClockDisplay>
+        <h2 class="text-lime-400 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          CST Timezone
+        </h2>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <ClockDisplay class="text-slate-400" time-zone="JST">
+        </ClockDisplay>
+        <h2 class="text-slate-400 text-center text-4xl text-shadow-[0_0_10px] text-shadow-current font-capsule tracking-widest">
+          JST Timezone
+        </h2>
+      </div>
+    </div>
+  </main>
+
+  <footer class="my-10 px-4 font-capsule tracking-widest">
+    <div class="container mx-auto">
+      <hr class="opacity-25 mb-8">
+      <p class="text-center text-2xl">
+        Copyright © 2025 All Rights Reserved -
+        <a href="https://github.com/Mahmoud217TR" target="__blank"
+          class="hover:text-amber-400 hover:text-shadow-[0_0_10px] hover:text-shadow-current transition-colors duration-300">
+          MahmoudTR
+        </a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      currentTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    }
+  },
+}
+</script>
---
+++ src/components/ClockDisplay.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+<template>
+  <div class="clock relative aspect-square w-48 bg-transparent border-2 border-gray-900 rounded-full flex font-digital
+      items-center justify-center text-current text-shadow-[0_0_10px] text-shadow-current shadow-black shadow-[0_0_20px]
+      select-none">
+    <span class="absolute top-0 start-0 w-full h-full border-transparent border-2 border-e-3 border-t-current
+    border-e-current rounded-full animate-[rotate_1s_linear_infinite]"
+      ></span>
+      <span class="block absolute top-1/2 start-1/2 w-1/2 h-1 bg-transparent origin-left
+        before:absolute before:w-4 before:aspect-square before:rounded-full before:bg-current before:-end-2 before:-top-1.5
+        before:shadow-[0_0_20px_5px] before:shadow-current animate-[circleRotate_1s_linear_infinite]">
+      </span>
+    <span class="text-4xl">
+      {{ time }}
+    </span>
+    <span class="spiner"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      now: new Date(),
+      timer: null
+    }
+  },
+  props: {
+    timeZone: null,
+  },
+  computed: {
+    time() {
+      return this.now.toLocaleTimeString(undefined, {timeZone: this.timeZone, hour: "2-digit", minute: "2-digit" })
+    }
+  },
+  mounted() {
+    this.timer = setInterval(() => (this.now = new Date()), 1000)
+  },
+  unmounted() {
+    clearInterval(this.timer)
+  }
+}
+</script>
+
+<style scoped>
+</style>
